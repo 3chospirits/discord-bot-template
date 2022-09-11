f20821c9b705d7a9ace1e9af920fe2dbdd0f4b48
IDcLuc <lucativio@example.com>
2022-09-11 22:49:47 +0200

    update to support v14 (this took way too long)

--- commands/info/help.js
+++ commands/info/help.js
@@ -1,72 +1,79 @@
 const Discord = require("discord.js");
+
+
 module.exports = {
 	name: "help",
 	aliases: ["h"],
 	category: "info",
 	description: "Returns all commands, or one specific command's info",
 	usage: "[command | alias]",
-	run: async (bot) => {
-		let {client, message, args, prefix} = bot;
-		if (args[0]) return getCMD(client, message, args[0]);
+	run: async ({client, message, args, prefix}) => {
+		message.reply({ ephemeral: true, content: "Lol" })
+		if (args[0]) return getCMD(client, message, args[0], prefix);
 		else return getAll(client, message, prefix);
 	},
 };
+
 function getAll(client, message, prefix) {
 	let reacts = [
         ":tools:",
         ":information_source:"
 	];
+
 	let embedfields = [];
+
 	client.categories.forEach(c => {
 		if (c == "hidden") return;
-		embedfields.push([
-			c,
-			client.commands
-				.filter(cmd => cmd.category === c)
+		embedfields.push({
+			name: c,
+			value: client.commands
+                .filter(cmd => cmd.category === c)
 				.map(cmd => `\`${cmd.name}\``)
 				.join(", "),
-		]);
+			inline: true
+		});
 	});
-	for (var i = 0; i < embedfields.length; i++) {
-		embedfields[i][0] = `${reacts[i]} ${embedfields[i][0][0].toUpperCase() +
-			embedfields[i][0].substring(1)}`;
+	
+	for (let i in embedfields) {
+		embedfields[i].name = `${reacts[i]} ${embedfields[i].name[0].toUpperCase() + embedfields[i].name.substring(1)}`;
 	}
-	//FIX change image to bot pfp auto link
-	var em = new Discord.MessageEmbed()
-		.setColor("RANDOM")
-		.setAuthor(
-			`Bot Commands`,
-			message.guild.iconURL({dynamic:true}),
-			""
-		);
-	embedfields.forEach(b => {
-		em.addField(b[0], b[1], true);
-	});
-	message.channel.send(
-		em.setFooter(
-			`Use ${prefix}help <command> for more info on a specific command\n[Command count: ` +
-				client.commands.array().length +
-				"]"
-		)
-	);
-}
-function getCMD(client, message, input) {
-	const embed = new Discord.MessageEmbed();
-	const cmd =
-		client.commands.get(input.toLowerCase()) ||
-		client.aliases.get(input.toLowerCase());
-	let info = `No information found for command **${input.toLowerCase()}**`;
-	if (!cmd)
-		//no specified command
-		return message.channel.send(embed.setColor("RED").setDescription(info));
 
-	if (cmd.name) info = `**Command name**: ${cmd.name}`;
-	if (cmd.aliases)
-		info += `\n**Aliases**: ${cmd.aliases.map(a => `\`${a}\``).join(", ")}`;
-	if (cmd.description) info += `\n**Description**: ${cmd.description}`;
-	if (cmd.usage) {
-		info += `\n**Usage**: ${cmd.usage}`;
-		embed.setFooter(`Syntax: <> = required, [] = optional`);
+	const em = {
+		title: "Bot Commands",
+		fields: embedfields,
+		footer: { text: `Use ${prefix}help <command> for more info on a specific command\n[Command count: ${client.commands.size} ]` },
 	}
-	return message.channel.send(embed.setColor("GREEN").setDescription(info));
+	message.channel.send({ embeds: [em] });
+}
+function getCMD(client, message, input, prefix) {
+	const embed = new Discord.EmbedBuilder();
+
+	let cmd;
+	
+	client.commands.forEach(command => {
+		if (command.name == input.toLowerCase() || command.aliases?.includes(input.toLowerCase())) {
+			return cmd = command
+		}
+	})
+	let info;
+	if (!cmd) {
+		//no specified command
+		info = `No information found for command **${input.toLowerCase()}**`
+		embed
+			.setColor("Red")
+			.setDescription(info)
+		return message.channel.send({ embeds: [embed] });
+	}
+	else {
+		if (cmd.name) info = `**Command name**: ${cmd.name}`;
+		if (cmd.aliases)
+			info += `\n**Aliases**: ${cmd.aliases.map(a => `\`${a}\``).join(", ")}`;
+		if (cmd.description) info += `\n**Description**: ${cmd.description}`;
+		if (cmd.usage) {
+			info += `\n**Usage**: ${prefix}${cmd.name} ${cmd.usage}`;
+			embed.setFooter({ text: `Syntax: <> = required, [] = optional` });
+		}
+		embed.setColor("Green").setDescription(info)
+		return message.channel.send({ embeds: [embed] });
+	}
 }
