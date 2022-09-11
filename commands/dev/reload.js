d29ced9274c1f2bcf83dbfa34322771864182b4b
IDcLuc <lucativio@example.com>
2022-09-11 22:47:27 +0200

    MessageEmbed -> EmbedBuilder

--- commands/dev/reload.js
+++ commands/dev/reload.js
@@ -5,25 +5,21 @@ module.exports = {
     category: "dev",
     adminOnly: true,
     description: "reloads the bot",
-    run: async (bot) => {
-        var { client, message, config } = bot;
+    run: async ({ client, message, config }) => {
 		await client.loadCmds(client, true); //reload is true
         await client.loadEvents(client, true);
         await client.loadFunctions(client, true);
-
-		message.channel.send(
-			new Discord.MessageEmbed()
-				.setColor("#8DC685")
-				.setTitle("Bot Reload Complete")
-				.setDescription(
-					client.functions.get("functions").autoAlign([
-						[`\`${client.commands.size}\``, `Commands`],
-						[`\`${client.aliases.size}\``, `Aliases`],
-						[`\`${client.events.size}\``, `Events`],
-						[`\`${client.functions.size}\``, `Functions`],
-					])
-				)
+		const embed = new Discord.EmbedBuilder()
+		.setColor("#8DC685")
+		.setTitle("Bot Reload Complete")
+		.setDescription(
+			client.functions.get("functions").autoAlign([
+				[`\`${client.commands.size}\``, `Commands`],
+				[`\`${client.aliases.size}\``, `Aliases`],
+				[`\`${client.events.size}\``, `Events`],
+				[`\`${client.functions.size}\``, `Functions`],
+			])
 		)
+		message.channel.send({ embeds: [embed] })
     }
-  }
-  
+}
