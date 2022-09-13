af36197e8cef42a7b013cb62bf0edc2d5d2f8220
IDcLuc <lucativio@example.com>
2022-09-13 13:06:10 +0200

    add s to bot_admin to make more sense change text in the properties

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    bot_admin: ["your-user-id-here"], //put the "admins" of the bot here, this will give them access to commands marked with `adminOnly: true`
-    token: "place-your-token-here", //bot's login token from the discord developer page
+    bot_admins: [""], //put the "admins" of the bot here, this will give them access to commands marked with `adminOnly: true`
+    token: "", //bot's login token from the discord developer page
     prefix: "." //prefix that the command calls should start with
 }
-//any changes to this file will require a full bot restart to update
+//any changes to this file will require a full bot restart to update 
